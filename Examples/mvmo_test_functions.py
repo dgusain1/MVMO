1ccd15ad360d4e8dab54bb6d8ef51d41dbc30d95
Digvijay <d.gusain@tudelft.nl>
2019-12-06 18:31:21 +0100

    updated example

--- Examples/mvmo_test_functions.py
+++ Examples/mvmo_test_functions.py
@@ -5,9 +5,6 @@ Created on Sat Nov 30 12:36:20 2019
 @author: digvijaygusain
 """
 
-import numpy as np
-from MVMO import MVMO
-
 from MVMO import MVMO
 from MVMO import test_functions
 function_rosen = test_functions.rosen
@@ -15,12 +12,12 @@ function_easom = test_functions.easom
 
 optimizer = MVMO(iterations=5000, num_mutation=3, population_size=10)
 
-def func_constr(x):
-	return True if x[0]**2 + x[1]**2 < 1 else False
+def func_constr(X):
+	return True if X[0]**2 + X[1]**2 < 1 else False
 	
 bds = [(-1.5,1.5), (-0.5,2.5)]
-constr = {'ineq':"(x[0] - 1)**3 - x[1] + 1",
-		  'eq':"x[0]+x[1]-2",
+constr = {'ineq':"(X[0] - 1)**3 - X[1] + 1",
+		  'eq':"X[0]+X[1]-2",
 		  'func':func_constr}
 res, conv, sol = optimizer.optimize(obj_fun=function_rosen, bounds=bds, constraints=constr)
 print(f'Rosenbrock optimum value = {res[0]} at X = {res[1]}')
