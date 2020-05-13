42f04460c688e6ad6346016f75c61ade5ecda7c2
dgusain1 <d.gusain@tudelft.nl>
2020-05-13 09:35:11 +0200

    ..

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def readme():
 
 setup(
       name='MVMO',
-      version='1.0.9',
+      version='1.0.10',
       description='Python package for heuristic optimization',
       long_description=readme(),
       long_description_content_type="text/markdown",
